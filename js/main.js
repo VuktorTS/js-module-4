7bab2ccbabdbfefdceaf500653f2476794a66b45
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-09-24 20:25:43 +0300

    20:25

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const numbers = [5, 8, 9, 10, 35, 78];
+// const numbers = [5, 8, 9, 10, 35, 78];
 
-numbers.forEach((number, index, array) => {
-  console.log(number);
-  console.log(index);
-  console.log(array);
-});
+// numbers.forEach((number, index, array) => {
+//   console.log("number: ", number);
+//   console.log("index: ", index);
+//   console.log("array: ", array);
+// });
+
+function foo(callback) {
+  callback(10);
+}
+
+function logger(value) {
+  console.log("value: ", value);
+}
+
+foo(logger);
