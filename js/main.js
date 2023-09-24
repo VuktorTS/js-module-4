ca4da5cab14125d776198d9ab6e66d42f8ee0179
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-09-24 20:54:53 +0300

    20:54

--- js/main.js
+++ js/main.js
@@ -6,12 +6,38 @@
 //   console.log("array: ", array);
 // });
 
-function foo(callback) {
-  callback(10);
+// function foo(callback) {
+//   callback(10);
+// }
+
+// function logger(value) {
+//   console.log("value: ", value);
+// }
+
+// foo(logger);
+
+const arr = [2, 6, 1, 7, 3];
+
+function each(arr, callback) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; i += 1) {
+    total = callback(total, arr[i]);
+  }
+  return total;
 }
 
-function logger(value) {
-  console.log("value: ", value);
+console.log(each(arr, add));
+console.log(each(arr, sum));
+console.log(each(arr, division));
+
+function add(first, second) {
+  return first + second;
 }
 
-foo(logger);
+function sum(first, second) {
+  return first * second;
+}
+
+function division(first, second) {
+  return first / second;
+}
